8d7302028a37464c5986e163bc0b805eab20014e
Ankur Prasad <ankurprasad711@gmail.com>
2021-02-15 21:53:28 +0530

    tasks completed

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ankurprasad711/myapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapi\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=mlapi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlapi 8000:80
